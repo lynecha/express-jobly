d97e3fdcdecf38fece3fe654b6e5e2ccfa5a713b
Michael Paglione <pagman777@gmail.com>
2022-04-22 13:38:08 -0400

    adujst server from pair

--- db.js
+++ db.js
@@ -4,26 +4,12 @@
 
 const { Client } = require("pg");
 
-const DB_URI = process.env.NODE_ENV === "test"
-    ? "postgres://postgres:1084314Lyne!@localhost:5432/jobly_test"
-    : "postgres://postgres:1084314Lyne!@localhost:5432/jobly";
-let db = new Client({
-  connectionString: DB_URI,
+const { getDatabaseUri } = require("./config");
+
+const db = new Client({
+  connectionString: getDatabaseUri(),
 });
 
 db.connect();
 
-
 module.exports = db;
-
-
-
-// const { getDatabaseUri } = require("./config");
-
-// const db = new Client({
-//   connectionString: getDatabaseUri(),
-// });
-
-// db.connect();
-
-// module.exports = db;
